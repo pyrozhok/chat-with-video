b718fac279c47ebe3ebe8ea4804e3e8725cab348
Aleksandr Sopin <alex.sopin0803@gmail.com>
2023-09-18 15:52:35 +0700

    add image preview and remove

--- components/file-upload.tsx
+++ components/file-upload.tsx
@@ -1,5 +1,8 @@
 "use client";
 
+import { X } from "lucide-react";
+import Image from "next/image";
+
 import { UploadDropzone } from "@/lib/uploadthing";
 
 import "@uploadthing/react/styles.css";
@@ -11,6 +14,27 @@ interface FileUploadProps {
 }
 
 export const FileUpload = ({ onChange, value, endpoint }: FileUploadProps) => {
+  const fileType = value.split(".").pop();
+  if (value && fileType !== "pdf") {
+    return (
+      <div className="relative h-20 w-20">
+        <Image
+          fill
+          src={value}
+          alt="upload"
+          quality={70}
+          className="rounded-full"
+        />
+        <button
+          onClick={() => onChange("")}
+          type="button"
+          className="bg-rose-500 text-white p-1 rounded-full absolute top-0 right-0 shadow-sm"
+        >
+          <X className="h-4 w-4" />
+        </button>
+      </div>
+    );
+  }
   return (
     <UploadDropzone
       endpoint={endpoint}
