832f4798d8156ab25dc2b0794bc858555cbf1e2b
Aleksandr Sopin <alex.sopin0803@gmail.com>
2023-09-26 20:51:28 +0700

    add modal of server creation

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,8 +2,11 @@ import "./globals.css";
 import type { Metadata } from "next";
 import { ClerkProvider } from "@clerk/nextjs";
 import { Inter } from "next/font/google";
-import { ThemeProvider } from "@/components/providers/theme-provider";
+
 import { cn } from "@/lib/utils";
+
+import { ThemeProvider } from "@/components/providers/theme-provider";
+import { ModalProvider } from "@/components/providers/modal-provider";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -25,8 +28,8 @@ export default function RootLayout({
             attribute="class"
             defaultTheme="dark"
             enableSystem
-            storageKey="chat-theme"
-          >
+            storageKey="chat-theme">
+            <ModalProvider />
             {children}
           </ThemeProvider>
         </body>
